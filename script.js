5d810f915429cf5d6d8c8193c329dd69e9397ee1
vikas9238 <vikashkumar10237@gmail.com>
2023-09-17 21:23:46 +0530

    home page > hero sec. changes

--- script.js
+++ script.js
@@ -54,3 +54,34 @@ function sendEmail() {
       document.getElementById("message").value,
   }).then((message) => alert("Message send Succesfully"));
 }
+
+
+/* GSAP STARTER */
+var tl = gsap.timeline()
+
+tl.from(".nav img, .nav li, .nav>.button-align",{
+  y:-80,
+  opacity:0,
+  duration:0.8,
+  delay:0.5,
+  stagger:0.3
+})
+
+tl.from(".main h1",{
+  y:100,
+  opacity:0,
+  stagger:0.3
+})
+tl.from(".main>img",{
+  scale:0,
+  opacity:0,
+  stagger:0.3
+})
+tl.from(".down-arrow",{
+  scale:0,
+  opacity:0
+})
+tl.to(".down-arrow",{
+    y:40,
+    repeat:-1
+})
